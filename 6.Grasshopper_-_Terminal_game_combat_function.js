f8615f4a31ef187e5b5fe3f125766ebcfcabc25e
Wijdan <wijdanbita@gmail.com>
2024-07-15 06:25:00 +0700

    Practice - 6

---
+++ 6.Grasshopper_-_Terminal_game_combat_function.js
@@ -0,0 +1,24 @@
+// Grasshopper - Terminal game combat function
+// Level : 8 kyu
+// link : https://www.codewars.com/kata/586c1cf4b98de0399300001d
+
+function combat(health, damage) {
+  // const result = health - damage;
+  // if (result < 0) {
+  //   return 0;
+  // }
+  // return result;
+
+  // ++ Persingkat ke 1 ++
+  return health - damage < 0 ? 0 : health - damage;
+
+  // ++Jawaban orang lain++
+  //  return Math.max(0, health - damage);
+}
+
+// ++ arrow function ++
+// const combat = (health, damage) => health - damage < 0 ? 0 : health - damage
+
+console.log(combat(100, 5));
+console.log(combat(92, 8));
+console.log(combat(20, 30));
