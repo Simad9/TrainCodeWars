dbfa21dd8a7bfc8cf79a0829886a5929f1b0c933
Wijdan <wijdanbita@gmail.com>
2024-08-04 07:19:00 +0700

    Practice - 26

---
+++ 26.Even_or_Odd.js
@@ -0,0 +1,10 @@
+// Even or Odd
+// Level : 8 kyu
+// link : https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+function evenOrOdd(number) {
+  // ++ Awal ketemu ++
+  return number % 2 == 0 ? "Even" : "Odd";  
+}
+
+console.log(evenOrOdd(7));
