1960716d43ffd9ccdce2eddfe22808bddca5a3c0
Wijdan <wijdanbita@gmail.com>
2024-08-17 08:03:04 +0700

    Practice - 39

---
+++ 39.Grasshopper_-_Terminal_Game_#1.js
@@ -0,0 +1,12 @@
+// Grasshopper - Terminal Game #1
+// Level : 8 kyu
+// link : https://www.codewars.com/kata/55e8aba23d399a59500000ce
+
+function Hero(name) {
+  //++ Awal ketemu ++
+  this.name = name || "Hero";
+  this.position = "00";
+  this.health = 100;
+  this.damage = 5;
+  this.experience = 0;
+}
